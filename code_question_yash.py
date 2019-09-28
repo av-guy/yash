20b96bc1586baa29554076f1e922c88f36ac0363
unknown <jonathan@pacificwestsound.com>
2019-09-28 12:20:13 -0700

    added loop

--- code_question_yash.py
+++ code_question_yash.py
@@ -26,26 +26,28 @@ def authorize_login_oauth():
     drive_service = discovery.build('drive', 'v3', http=creds.authorize(Http()))
     return drive_service
 
-def get_file_id(drive_service):
+def get_file_ids(drive_service):
     # You can take this out of the loop. I just used it to pull the first file
     # I could find on my drive.
     results = drive_service.files().list(
         pageSize=10,
         fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
+    files_to_download = []
     if not items:
         print('Nothing here!')
     else:
         print('Files:')
         for item in items:
+            files_to_download.append((item['name'], item['id']))
             print(u'{0} ({1})'.format(item['name'], item['id']))
-    return items[0]['id']
+    return files_to_download
 
-def download_file(drive_service, file_id):
+def download_file(drive_service, file_id, file_name):
     # Using files().get per SO
     # --> https://stackoverflow.com/questions/46302540/google-drive-export-non-google-doc-file
     request = drive_service.files().get(fileId=file_id)
-    fh = io.FileIO('test.pdf', 'wb')
+    fh = io.FileIO('downloads/' + file_name, 'wb')
     downloader = MediaIoBaseDownload(fh, request)
     done = False
     while done is False:
@@ -54,5 +56,6 @@ def download_file(drive_service, file_id):
 
 if __name__ == '__main__':
     drive_service = authorize_login_oauth()
-    target_file = get_file_id(drive_service)
-    print(download_file(drive_service, target_file))
+    target_files = get_file_ids(drive_service)
+    for file in target_files:
+        download_file(drive_service, file[1], file[0])
